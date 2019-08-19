84f565d3257e9805b072ec15a26e7dd4b79ee25e
Cortess <CortessHack@gmail.com>
2019-08-19 15:03:41 +0300

    HTTP Requests with Axios

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
-import List from './components/List';
+
+window.axios = require('axios');
 
 new Vue({
     el: '#app',
 
     components: {
-      List,
+
+    },
+
+    mounted: function () {
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => this.posts = response.data)
+            .catch(error => this.posts = [{ title: 'No posts found'}])
+            .finally(() => console.log('Data loading complete'));
+
     },
 
     data: {
-
+        posts: null,
     },
 
 
