1831e219ddd08c74c75c2a04cd8a70c5249b9cb3
Cortess <CortessHack@gmail.com>
2019-08-19 14:21:14 +0300

    Passing data between custom components

---
+++ src/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p v-text="dropdownResult"></p>
+
+        <dropdown @update:option="updateOption"></dropdown>
+    </div>
+</template>
+
+<script>
+    import Dropdown from './Dropdown.vue';
+
+    export default {
+        name: "List",
+
+        data: function () {
+            return {
+                dropdownResult: '',
+            }
+        },
+
+        components: {
+            Dropdown,
+        },
+
+        methods: {
+            updateOption: function (value) {
+                this.dropdownResult = value;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
