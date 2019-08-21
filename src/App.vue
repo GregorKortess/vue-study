115d981bb6d29768d62f07a66c601972668eb0d3
Cortess <CortessHack@gmail.com>
2019-08-21 08:43:50 +0300

    Class and style binding

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,57 @@
 <template>
-  <div id="app">
-    <tasks/>
-  </div>
+    <div id="app">
+        <div :class="classObject">{{ value }}</div>
+        <div :style="{ fontSize: value + 'px'}">{{ value }}</div>
+
+        <button type="button" @click="value++">+</button>
+        <button type="button" @click="value--">-</button>
+        <button type="button" @click="error = ! error">Error</button>
+        <button type="button" @click="warning = ! warning">Warning</button>
+    </div>
 </template>
 
 <script>
-import Tasks from './components/Tasks.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Tasks
-  }
-}
+    export default {
+        name: 'app',
+
+        data: function () {
+            return {
+                value: 1,
+                error: false,
+                warning: false,
+            }
+        },
+
+        computed: {
+            classObject: function () {
+                return {
+                    'red-error': this.value > 5 ,
+                    'green-error': this.value <= 5 ,
+                    'yellow-warning': this.warning
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        font-size: 40px;
+        text-align: center;
+        padding-top: 100px;
+    }
+
+    .red-error {
+        background-color: darkred;
+    }
+
+    .green-error {
+        background-color: darkgreen;
+    }
+
+
+    .yellow-warning {
+        text-decoration: underline;
+    }
 </style>
