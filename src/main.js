116313bd2cdb7b80beb09849cc03b0e8f5d78977
Cortess <CortessHack@gmail.com>
2019-08-21 10:26:16 +0300

    Simple routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Tasks from './components/Tasks.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import NotFound from './components/NotFound.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const routes = {
+  '/': App,
+  '/test' : Tasks,
+  '/hello' : HelloWorld,
+    };
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+  data: {
+    currentRoute: window.location.pathname,
+  },
+
+  computed: {
+    currentComponent: function () {
+      return routes[this.currentRoute] || NotFound;
+    }
+  },
+
+  render: function (h) {
+    return h(this.currentComponent)
+  },
+}).$mount('#app');
